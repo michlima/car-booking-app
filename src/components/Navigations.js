5161c915ac51c26b38ac7f9d14e2fd39652fb111
Michael Lima <michaellima@Michaels-MBP.fritz.box>
2022-10-31 12:34:49 +0100

    sign out option

--- src/components/Navigations.js
+++ src/components/Navigations.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import {FaCarAlt} from 'react-icons/fa'
+import {FaCarAlt, FaSignOutAlt} from 'react-icons/fa'
 import {BsFillCalendar2DayFill} from 'react-icons/bs'
+import { auth } from '../backend/firebase'
 
 
 
-const Navigation = () => {
-    let buttoncls = 'duration-200 mx-4 w-16 bg-gray-800 text-white h-16 rounded-full flex items-center justify-center'
+const Navigation = (props) => {
+    let buttoncls = ' border-b-4 border-primary-2 duration-200 translate-y-4 mx-4 w-16 bg-gray-800 text-white h-16 rounded-full flex items-center justify-center'
+
+    const signOut = () => {
+        auth.signOut()
+    }
+    
 
     return(
-        <div className='duration-200 w-full flex items-center justify-center h-40 bg-white rounded-b-lg'>
+        <div className='duration-200 w-full flex items-center justify-center h-12 mb-10 bg-gray-800'>
             <Link to='/' className={buttoncls} >
-                <FaCarAlt size={40}/>
+                <FaCarAlt size={30}/>
             </Link>
             <Link to='calendar' className={buttoncls}>
-                <BsFillCalendar2DayFill size={40}/>
+                <BsFillCalendar2DayFill size={30}/>
             </Link>
+            <button className={buttoncls}   onClick={signOut}><FaSignOutAlt size={30}/></button>
         </div>
     )
 }
