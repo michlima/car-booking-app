4fab7c34ac3349d7b01b7674a674cb2a0d0c1c4b
Michael Lima <michaellima@Michaels-MBP.fritz.box>
2022-10-31 12:33:00 +0100

    authentication

--- src/backend/utils.js
+++ src/backend/utils.js
@@ -1,23 +1,27 @@
 import { collection, query, where,doc, setDoc ,getDocs, addDoc } from "firebase/firestore"; 
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "./firebase";
+import { createUserWithEmailAndPassword, signOut} from "firebase/auth";
+
 
 
 import { db } from "./firebase";
 
 const bookTime = async (data) => {
-   
     try {
         const docRef = await addDoc(collection(db, 'schedule',), {
-          driver      : data.driver,
-          destination : data.destination,
-          reason      : data.reason,
-          startHour   : data.startHour,
-          startMinute : data.startMinute,
-          endHour     : data.endHour,
-          endMinute   : data.endMinute,
-          month       : data.month,
-          day         : data.day,
-          year        : data.year,
-          car         : data.car
+          reservationId : data.id,
+          driver        : data.driver,
+          destination   : data.destination,
+          reason        : data.reason,
+          startHour     : data.startHour,
+          startMinute   : data.startMinute,
+          endHour       : data.endHour,
+          endMinute     : data.endMinute,
+          month         : data.month,
+          day           : data.day,
+          year          : data.year,
+          car           : data.car
         });
         console.log("Document written with ID: ", docRef.id);
       } catch (e) {
@@ -46,6 +50,53 @@ const getReservations = async () => {
   return data
 }
 
+const signIn = (credentials) => {
+  signInWithEmailAndPassword(auth, credentials.email, credentials.password)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            console.log(user)
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+        });
+}
+
+const register = (email, userInfo) => {
+  createUserWithEmailAndPassword(auth, email, userInfo.password)
+  .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      registerData(user.uid, email,userInfo.firstName, userInfo.lastName, userInfo.age)
+      console.log(user)
+      // ...
+  })
+  .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+  });
+}
+
+const registerData = async (uid,email, fName, lName, age) => {
+  const dataUid = {
+    email: email,
+    firstName: fName,
+    lastName: lName,
+    age: age,
+  }
 
 
-export {bookTime, getReservations, writeKms}
+  try {
+    await setDoc(doc(db, "users", uid), dataUid);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+    return false
+  }
+  return true
+}
+
+
+
+export {bookTime, getReservations, writeKms, signIn, register}
