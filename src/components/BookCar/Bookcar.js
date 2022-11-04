9a1c2b7f08f3684afc2a23a9278bed462c29574d
Michael Lima <michaellima@Michaels-MBP.fritz.box>
2022-11-04 13:11:45 +0100

    Organiztions: New Reservation component

--- src/components/BookCar/Bookcar.js
+++ src/components/BookCar/Bookcar.js
@@ -1,9 +1,8 @@
 import React, { useRef, useState } from 'react'
 import Calendar from './Calendar'
-import {FaCarAlt} from 'react-icons/fa'
-import {GoLocation} from 'react-icons/go'
 import ModalBookCar from './ModalBookCar'
 import FocusReservation from './FocusReservation'
+import ReservationComponent from './ReservationComponent'
 
 const cars = [
     'Blue Van',
@@ -107,32 +106,12 @@ const Bookcar = (props) => {
             {props.schedule ? 
                 <div className='grid grid-cols-2 w-screen flex items-center justify-center mt-2'>
                     {props.schedule.map((object, index) => {
-                        let schedule = object.data
-                        if(schedule.month == data.month && schedule.day == data.day && schedule.year == data.year){
-                                let cls
-                                switch (schedule.car) {
-                                    case 'Blue Van':
-                                        cls = 'relative flex flex-col bg-blue-500 m-4 text-white rounded-lg shadow-lg'
-                                        break;
-                                    case 'Truck':
-                                        cls = 'relative flex flex-col bg-white m-4 rounded-lg text-black shadow-lg'
-                                        break;
-                                    default:
-                                        cls = 'relative flex flex-col bg-gray-400 m-4 text-white rounded-lg shadow-lg'
-
-                                }
-                            
-                            return(
-                                <button onClick={() => showReservationData(object)} key={index} className={cls}>
-                                    <div className='bg-gray-800 w-full flex flex-col text-1xl rounded-t-lg p-2 items-center text-white'>
-                                        <span className='mr-2 text-1xl font-bold'>{schedule.driver}</span>
-                                        {`${schedule.startHour}:${schedule.startMinute} - ${schedule.endHour}:${schedule.endMinute}`}
-                                    </div> 
-                                    <p className=' mx-2 mt-1 flex items-center justify-center font-semibold'><GoLocation className='m-2'/>{schedule.destination}</p>
-                                    <p className=' mx-2 my-1 flex items-center justify-center font-semibold '><FaCarAlt className='m-2'/>{schedule.car}</p>
-                                </button> 
-                            )
-                        }
+                        return(
+                            <ReservationComponent 
+                                schedule={object.data} 
+                                data={data}
+                                showReservation={() => setShowReservation(true)}/>
+                        )
                     })}
                     
                 </div>
---
+++ src/components/BookCar/ReservationComponent.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import {FaCarAlt} from 'react-icons/fa'
+import {GoLocation} from 'react-icons/go'
+
+const ReservationComponent = (props) => {
+    const schedule = props.schedule
+    const data = props.data
+    let cls
+
+    console.log(props)
+
+    if(schedule.month == data.month && schedule.day == data.day && schedule.year == data.year){
+        switch (schedule.car) {
+            case 'Blue Van':
+                cls = 'relative flex flex-col bg-blue-500 m-4 text-white rounded-lg shadow-lg'
+                break;
+            case 'Truck':
+                cls = 'relative flex flex-col bg-white m-4 rounded-lg text-black shadow-lg'
+                break;
+            default:
+                cls = 'relative flex flex-col bg-gray-400 m-4 text-white rounded-lg shadow-lg'
+    
+        }
+    
+    return(
+        <button onClick={() => props.showReservationData(true)} className={cls}>
+            <div className='bg-gray-800 w-full flex flex-col text-1xl rounded-t-lg p-2 items-center text-white'>
+                <span className='mr-2 text-1xl font-bold'>{schedule.driver}</span>
+                {`${schedule.startHour}:${schedule.startMinute} - ${schedule.endHour}:${schedule.endMinute}`}
+            </div> 
+            <p className=' mx-2 mt-1 flex items-center justify-center font-semibold'><GoLocation className='m-2'/>{schedule.destination}</p>
+            <p className=' mx-2 my-1 flex items-center justify-center font-semibold '><FaCarAlt className='m-2'/>{schedule.car}</p>
+        </button> 
+    )
+    }
+        
+}
+
+export default ReservationComponent
