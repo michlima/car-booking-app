04ca38036bf2a38d222477adb698360424e0d119
Michael Lima <michaellima@Michaels-MBP.fritz.box>
2022-11-07 17:05:31 +0100

    moved clock in foccused reservation

--- src/components/BookCar/FocusReservation.js
+++ src/components/BookCar/FocusReservation.js
@@ -5,17 +5,33 @@ import {GoLocation} from 'react-icons/go'
 import {CgArrowsExchange} from 'react-icons/cg'
 import {BiTime} from 'react-icons/bi'
 import {GiPathDistance} from 'react-icons/gi'
+import {AiFillEdit} from 'react-icons/ai'
 import {RiSendPlane2Fill, RiCloseCircleLine} from 'react-icons/ri'
 import {MdOutlineArrowBackIos} from 'react-icons/md'
 import Input from '../Input'
-import { writeKms } from '../../backend/utils'
+import { writeKms, editTime} from '../../backend/utils'
+import Clock from './Clock'
+import { Link, useLocation, useNavigate} from 'react-router-dom'
 
-const modalOpen = 'duration-200 overflow-auto pb-20 fixed bottom-0 w-screen h-full flex justify-enter items-center flex-col bg-opacity-75 backdrop-blur-lg bg-slate-300 select-none'
-const modalClosed = 'duration-200 w-0 h-0 fixed bottom-0 right-0'
+const cls = 'duration-200 overflow-auto pb-20 fixed bottom-0 w-screen h-full flex justify-enter items-center flex-col bg-gray-800 select-none'
+
+const hours = ['12']
+for(let i = 0; i < 24;i++){
+    if(i == 11){
+        hours.push('00')
+        continue
+    }
+    hours.push(`${i + 1}`)
+}
 
 
 const FocusReservation = (props) => {
-    let modalCls = props.isShowing ? modalOpen : modalClosed
+    const navigate = useNavigate()
+    const location = useLocation()
+    console.log(location.state)
+    const reservationId = location.state.reservationData.id
+    const data = location.state.reservationData.data
+    console.log(data)
     const [kmsFetched, setKmsFetched] = useState({
         start: false,
         end: false
@@ -24,6 +40,28 @@ const FocusReservation = (props) => {
         start: '',
         end: ''
     })
+    const [editting, setEditing] = useState({
+        kmsStart: false,
+        kmsEnd: false,
+        timeStart: false,
+        timeEnd: false,
+    })
+     
+    const [time, setTime] = useState({
+        startHour: data.startHour,
+        startMinute: data.startMinute,
+        endHour: data.endHour,
+        endMinute: data.endMinute,
+    })
+    const [value, setvalue] = useState()
+
+    console.log(time)
+
+
+    const [selectingHour, setSelectingHour] = useState(true)
+    const [am, setIsAm] = useState(true)
+    const [startSelect, setStartSelect] = useState(false)
+    const [endSelect, setEndSelect] = useState(false)
 
             
     const getkms = async (id) => {
@@ -36,11 +74,8 @@ const FocusReservation = (props) => {
         }
     }
     useEffect(() => {
-        if(props.reservation){
-            getkms(props.reservation.id)
-        }
-        
-    }, [props.reservation])
+        getkms(reservationId)
+    }, [])
 
     const handleInput = (name, value) => {
         setFillKms(prevState => ({
@@ -54,22 +89,118 @@ const FocusReservation = (props) => {
             ...prevState,
             [name]:value
         }))
-        writeKms(fillKms, props.reservation.id)
+        writeKms(fillKms, reservationId)
 
     } 
-    
 
+    const handleClock = (type, value) => {
+        if(type == 'startHour'){
+            handleClockStart('startHour', value)
+        }
+        if(type == 'startMinute'){
+            handleClockStart('startMinute', value)
+        }
+        if(type == 'endHour'){
+            handleClockEnd('endHour', value)
+        }
+        if(type == 'endMinute'){
+            handleClockEnd('endMinute', value)
+        }
+    }
+
+    const handleClockEnd = (name,hour) => {
+        let value = hour
+        if(name == 'endHour'){
+            console.log('oops')
+            setSelectingHour(false)
+            if(Number(hour) < Number(time.startHour)){
+                console.log('oops')
+                value = Number(value) + 12
+            }
+        } else {
+            setEditing( prevValue =>({
+                ... prevValue,
+                'timeEnd' : false
+            }))
+            setSelectingHour(true)
+            let selectedTime = {endHour: time.endHour, endMinute: hour}
+        }
+        
+        if(!am && name == 'endHour'){
+            if(value == 12){
+                value = hours[hour]    
+            }else {
+                let i = +hour + 12
+                value = hours[+hour + 12]
+            }
+        }
+
+        setTime(prevState => ({
+            ...prevState,
+            [name] : value
+        }))
+        editTime(reservationId, name, value)
+
+    }
+
+    const handleClockStart = (name,hour) => {
+        let value = hour
+
+        if(!am && name == 'startHour'){
+            if(value == 12){
+                value = hours[hour]    
+            } else {
+                value = hours[+hour + 12]
+            }
+        }
+        setTime(prevState => ({
+            ...prevState,
+            [name] : value
+        }))
+
+        if(name == 'startHour'){
+            setSelectingHour(false)
+        } else {
+            setEditing( prevValue =>({
+                ... prevValue,
+                'timeStart' : false
+            }))
+            setSelectingHour(true)
+        }
+        editTime(reservationId, name, value)
+    }
+    
+    const editThis = (name, value) => {
+        setSelectingHour(true)
+        if(name == 'timeStart'){
+            setEditing( prevValue =>({
+                ... prevValue,
+                [name]: value,
+                'timeEnd' : false
+            }))
+        }
+        if(name == 'timeEnd'){
+            setEditing( prevValue =>({
+                ... prevValue,
+                [name]: value,
+                'timeStart' : false
+            }))
+        }
+        
+    }
+
+    const refreshData = () => {
+        props.getData()
+        navigate('/')
+    }
 
 
     return(
-        <div className={modalCls} >
-            <button onClick={props.closeModal} className='absolute flex items-center justify-center top-1 left-3 w-12 h-12 mt-2 text-white '><MdOutlineArrowBackIos size={20}/> </button>
-            { modalCls == modalClosed ?
-                <div/>
-                :
+        <div className={cls} >
+            <button onClick={refreshData} className='absolute flex items-center justify-center top-1 left-3 w-12 h-12 mt-2 text-white '><MdOutlineArrowBackIos size={20}/> </button>
                 <div className='w-screen '>
                     <div className='w-screen bg-gray-800   flex justify-enter items-center  pb-4 flex-col'>
-                        <a className='text-2xl text-white mt-5'>{props.reservation.data.driver}</a>
+                        <a className='text-2xl text-white mt-5'>{data.driver}</a>
                     </div>
                     <div className='bg-gray-800 w-11/12 rounded-lg m-3 text-white text-sm flex flex-row items-center justify-center'>
                         <div className='flex flex-col items-center justify-center'>
@@ -77,22 +208,46 @@ const FocusReservation = (props) => {
                         </div>
                         <CgArrowsExchange className='text-white mx-5 ' size={40}/>
                         <div className='flex flex-col items-center justify-center'>
-                            <a className=' mx-1 mt-1 flex items-center justify-center font-semibold'><GoLocation className='m-2'/>{props.reservation.data.destination ? props.reservation.data.destination :'??????'}</a>
+                            <a className=' mx-1 mt-1 flex items-center justify-center font-semibold'><GoLocation className='m-2'/>{data.destination ? data.destination :'??????'}</a>
                         </div>
                     </div>
                     <div className='bg-gray-800 p-5 w-11/12 rounded-lg m-3 text-white flex flex-col items-center justify-center'>
                         <a className='m-2 flex flex-col items-center'>
-                            Reason:<span className=' font-bold'>{props.reservation.data.reason}</span>
+                            Reason:<span className=' font-bold'>{data.reason}</span>
                         </a>
                         <div className='flex flex-row items-center justify-center'>
                             <div className='flex flex-col items-center justify-center'>
-                                <a className='mx-2 mt-1 text-3xl flex items-center justify-center font-semibold'>Start</a>
-                                <a className=' mx-2 mt-1 flex items-center justify-center font-semibold'>{props.reservation.data.startHour}:{props.reservation.data.startMinute}</a>                            
+                                <a className='mx-2 mt-1 flex items-center justify-center font-semibold'>Start</a>
+                                {
+                                    editting.timeStart 
+                                    ?
+                                
+                                        <Clock hours={hours} hour={selectingHour} am={am}setIsAm={() => setIsAm(!am)} handleClock={selectingHour ? ( value) => handleClock('startHour', value) :( value) => handleClock('startMinute', value) }/>  
+                                    :
+                                    <div className='flex flex-row items-center justify-center'>
+                                        <a className=' mx-2 mt-1 text-3xl flex items-center justify-center font-semibold'>{time.startHour}:{time.startMinute}</a>                            
+                                        <button onClick={() => editThis('timeStart', true)}>
+                                            <AiFillEdit size={20} className='text-primary-2'/>
+                                        </button>
+                                    </div>
+                                }
+                                
                             </div>
                             <CgArrowsExchange className='text-white mx-10' size={40}/>
                             <div className='flex flex-col items-center justify-center'>
-                                <a className=' mx-2 mt-1 text-3xl flex items-center justify-center font-semibold'>End</a>
-                                <a className=' mx-2 mt-1 flex items-center justify-center font-semibold'>{props.reservation.data.endHour}:{props.reservation.data.endMinute}</a>
+                                <a className=' mx-2 mt-1 flex items-center justify-center font-semibold'>End</a>
+                                {editting.timeEnd
+                                ?
+                                    <Clock hours={hours} hour={selectingHour} am={am}setIsAm={() => setIsAm(!am)} handleClock={selectingHour ? ( value) => handleClock('endHour', value) :( value) => handleClock('endMinute', value) }/>  
+                                :
+                                <div className='flex flex-row items-center justify-center'>
+                                    <a className=' mx-2 mt-1 text-3xl flex items-center justify-center font-semibold'>{time.endHour}:{time.endMinute}</a>
+                                    <button onClick={() => editThis('timeEnd', true)}>
+                                        <AiFillEdit size={20} className='text-primary-2'/>
+                                    </button>
+                                </div>
+                                }
+                                
                             </div>
                         </div>
                         <BiTime size={40}/>
@@ -135,27 +290,28 @@ const FocusReservation = (props) => {
                             </div>
                         </div>
                         <GiPathDistance className='m-2' size={40}/>
-                        {kmsFetched.start 
+                        {!kmsFetched.start ||  editting.kmsStart
                         ?
-                            <></>
-                        :
                             <div className='flex flex-row w-full items-center justify-center'>
-                                <Input label='start' placeholder='start kms...'  handleInput={(name, value) => handleInput(name, value)}/>
+                                <Input label='inputTimeStart' placeholder='start kms...'  handleInput={(name, value) => handleInput(name, value)}/>
                                 <button  onClick={() => confirmKms('start', fillKms.start)}className='flex -translate-y-2 ml-2'><RiSendPlane2Fill size={30}/></button>
                             </div>
-                        }
-                        {kmsFetched.end 
-                        ?
-                            <></>
                         :
+                            <></>
+                            
+                        }
+                        {!kmsFetched.end || editting.kmsEnd
+                        ?
                             <div className='flex flex-row w-full items-center justify-center'>
                                 <Input label='end' placeholder='end kms...'  handleInput={(name, value) => handleInput(name, value)}/>
                                 <button onClick={() => confirmKms('end', fillKms.end)} className=' flex -translate-y-2 ml-2'><RiSendPlane2Fill size={30}/></button>
                             </div>
+                        :
+                            <></>
                         }
                     </div>
                 </div>
-            }
+            <button onClick={() => setvalue(value => value +1 )}>refresh</button>
         </div>
     )
 }
