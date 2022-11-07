6b1de29e9dd626a59778f27280d65fa06e73cf63
Michael Lima <michaellima@Michaels-MBP.fritz.box>
2022-11-07 17:10:14 +0100

    removed modal / now as page

--- src/components/BookCar/ReservationComponent.js
+++ src/components/BookCar/ReservationComponent.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {FaCarAlt} from 'react-icons/fa'
 import {GoLocation} from 'react-icons/go'
+import { Link } from 'react-router-dom'
 
 const ReservationComponent = (props) => {
     const schedule = props.schedule
@@ -10,25 +11,25 @@ const ReservationComponent = (props) => {
     if(schedule.month == data.month && schedule.day == data.day && schedule.year == data.year){
         switch (schedule.car) {
             case 'Blue Van':
-                cls = 'relative flex flex-col bg-blue-500 m-4 text-white rounded-lg shadow-lg'
+                cls = 'flex flex-col bg-blue-500 m-4 text-white rounded-lg w-10/12 shadow-lg'
                 break;
             case 'Truck':
-                cls = 'relative flex flex-col bg-white m-4 rounded-lg text-black shadow-lg'
+                cls = 'relative flex flex-col bg-white m-4 rounded-lg text-black w-10/12 shadow-lg'
                 break;
             default:
-                cls = 'relative flex flex-col bg-gray-400 m-4 text-white rounded-lg shadow-lg'
+                cls = 'relative flex flex-col bg-gray-400 m-4 text-white rounded-lg w-10/12 shadow-lg'
     
         }
     
     return(
-        <button onClick={() => props.showReservation()} className={cls}>
+        <Link to='/focus-reservation' state={{ userid: props.userid, reservationData: props.object}} className={cls}>
             <div className='bg-gray-800 w-full flex flex-col text-1xl rounded-t-lg p-2 items-center text-white'>
                 <span className='mr-2 text-1xl font-bold'>{schedule.driver}</span>
                 {`${schedule.startHour}:${schedule.startMinute} - ${schedule.endHour}:${schedule.endMinute}`}
             </div> 
-            <p className=' mx-2 mt-1 flex items-center justify-center font-semibold'><GoLocation className='m-2'/>{schedule.destination}</p>
-            <p className=' mx-2 my-1 flex items-center justify-center font-semibold '><FaCarAlt className='m-2'/>{schedule.car}</p>
-        </button> 
+            <p className=' mx-2 mt-1 flex items-center font-semibold'><GoLocation className='m-2'/>{schedule.destination}</p>
+            <p className=' mx-2 my-1 flex items-center font-semibold '><FaCarAlt className='m-2'/>{schedule.car}</p>
+        </Link> 
     )
     }
         
